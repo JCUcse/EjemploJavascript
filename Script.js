301280ff47031412cca7e726ed9b6dddb3e0136e
JCUcse <j2457830@gmail.com>
2023-07-01 14:58:50 -0300

    Agrego comentarios

--- Script.js
+++ Script.js
@@ -1,17 +1,27 @@
+// Declaración de variables
 let párrafo;
 let spanEstilo;
 let numero;
+
+// Agregar evento de escucha para la tecla presionada
 document.body.addEventListener("keypress", (e) => {
-    document.body.innerHTML="";
-    párrafo=document.createElement("p");
-    párrafo.textContent="Tu presionaste la tecla ";
-    document.body.appendChild(párrafo);
-    spanEstilo=document.createElement("span");
-    spanEstilo.textContent=e.key.toUpperCase();
-    spanEstilo.style.color="#FCFF00";
-    párrafo.appendChild(spanEstilo);
-    numero=document.createElement("p");
-    numero.style.color="#FF0400"
-    numero.textContent=+e.keyCode;
-    document.body.appendChild(numero);
+  // Limpiar el contenido del body
+  document.body.innerHTML = "";
+
+  // Crear un elemento de párrafo
+  párrafo = document.createElement("p");
+  párrafo.textContent = "Presionaste la tecla ";// Agregar texto al elemento parrafeo
+  document.body.appendChild(párrafo);
+
+  // Crear un elemento de span para mostrar la tecla presionada en mayúsculas
+  spanEstilo = document.createElement("span");
+  spanEstilo.textContent = e.key.toUpperCase();// Convertir la tecla presiona a mayúscula 
+  spanEstilo.style.color = "#FCFF00"; // Establecer color de texto amarillo
+  párrafo.appendChild(spanEstilo);
+
+  // Crear un elemento de párrafo para mostrar el código de la tecla
+  numero = document.createElement("p");
+  numero.style.color = "#FF0400"; // Establecer color de texto rojo
+  numero.textContent = e.keyCode;// Agregar el código de tecla al elemento parrafeo
+  document.body.appendChild(numero);
 });
